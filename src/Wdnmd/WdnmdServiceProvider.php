69bb59894f284670ff29d80bd891064c2770ecd4
一个路过的假面骑士 <1370571528@qq.com>
2021-04-27 02:52:37 +0800

    ss

--- src/Wdnmd/WdnmdServiceProvider.php
+++ src/Wdnmd/WdnmdServiceProvider.php
@@ -10,8 +10,10 @@ class WdnmdServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->publishes([
-            __DIR__."/config/wdnmd.php" => config_path("wdnmd.php"),
-        ], "wdnmd-config");
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . "/config/wdnmd.php" => config_path("wdnmd.php"),
+            ], "wdnmd-config");
+        }
     }
 }
